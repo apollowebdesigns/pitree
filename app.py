a126e51c25b31eee1a9a2cdb571d0b13f5df5be6
Andy Evans <andrewevans0@outlook.com>
2018-09-16 13:56:47 +0100

    added new files and skeleton app

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 from serialtest import get_dist
 app = Flask(__name__)
+from tree import light_tree, dim_tree
 
 @app.route('/start')
 def start():
-   get_dist()
-   return 'Hello World'
+    get_dist()
+    return 'Hello World'
 
 @app.route('/stop')
 def stop():
-   return 'Hello World'
+    return 'Hello World'
+
+@app.route('/light')
+def light():
+    light_tree()
+    return 'Hello World'
+
+@app.route('/dim')
+def dim():
+    dim_tree()
+    return 'Hello World'
+
+@app.route("/")
+def app():
+   templateData = {}
+   return render_template('index.html', **templateData)
 
 
 if __name__ == '__main__':
-   app.run()
+    app.run()
