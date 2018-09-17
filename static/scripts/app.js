a728702189f8b2ee66b0df256a60bb9a51b75ca3
Andy Evans <andrewevans0@outlook.com>
2018-09-17 19:54:42 +0100

    added classes to the buttons

---
+++ static/scripts/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("p").click(function(){
+        $(this).hide();
+    });
+
+    $("p").click(function(){
+        $(this).hide();
+    });
+
+    $("p").click(function(){
+        $(this).hide();
+    });
+
+    $("p").click(function(){
+        $(this).hide();
+    });
+});
