5e20fbca1c616ec960c7a72593b4b577b339b4b0
Andy Evans <andrewevans0@outlook.com>
2018-09-21 17:23:33 +0100

    added more debugging code

--- nanpytest/nanpytest.py
+++ nanpytest/nanpytest.py
@@ -1,8 +1,6 @@
 from nanpy import ArduinoApi
 from nanpy import SerialManager
 from time import sleep
-import pprint
-pp = pprint.PrettyPrinter(width=41, compact=True)
 
 connection = SerialManager(device='/dev/ttyUSB0')
 
@@ -13,29 +11,24 @@ a = ArduinoApi(connection=connection)
 a.pinMode(trigPin, a.OUTPUT)
 a.pinMode(echoPin, a.INPUT)
 
-print('what is in the arduino api?')
-pp.pprint(a)
+def test():
+    a.digitalWrite(trigPin, a.LOW)
+    sleep(2/10000)
+    # Sets the trigPin on HIGH state for 10 micro seconds
+    a.digitalWrite(trigPin, a.HIGH)
+    sleep(1/10000)
+    a.digitalWrite(trigPin, a.LOW)
+    # Reads the echoPin, returns the sound wave travel time in microseconds
+    duration = a.pulseIn(echoPin, a.HIGH)
+    print('what is the duration?')
+    print(duration)
+    # Calculating the distance
+    distance = duration*0.034/2
+    print('what is the distance?')
+    print(distance)
+    # Prints the distance on the Serial Monitor
+    print("Distance: ")
+    print(distance)
 
-a.digitalWrite(trigPin, a.LOW)
-sleep(2/10000)
-# Sets the trigPin on HIGH state for 10 micro seconds
-a.digitalWrite(trigPin, a.HIGH)
-sleep(1/10000)
-a.digitalWrite(trigPin, a.LOW)
-# Reads the echoPin, returns the sound wave travel time in microseconds
-duration = a.pulseIn(echoPin, a.HIGH)
-print('what is the duration?')
-print(duration)
-# Calculating the distance
-distance = duration*0.034/2
-print('what is the distance?')
-print(distance)
-# Prints the distance on the Serial Monitor
-print("Distance: ")
-print(distance)
-
-# for i in range(0, 8):
-#     a.digitalWrite(13, a.HIGH)
-#     sleep(2)
-#     a.digitalWrite(13, a.LOW)
-#     sleep(2)
+while True:
+    test()
