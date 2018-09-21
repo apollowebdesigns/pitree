83b777dc6f509534f21a029adce1ac08483e4258
Andy Evans <andrewevans0@outlook.com>
2018-09-21 18:31:58 +0100

    testing the tree

--- nanpytest/nanpytest.py
+++ nanpytest/nanpytest.py
@@ -1,6 +1,11 @@
 from nanpy import ArduinoApi
 from nanpy import SerialManager
 from time import sleep
+import pprint
+from time import sleep
+from gpiozero import LEDBoard
+from gpiozero.tools import random_values
+from signal import pause
 
 connection = SerialManager(device='/dev/ttyACM0')
 
@@ -8,16 +13,29 @@ trigPin = 9
 echoPin = 10
 
 a = ArduinoApi(connection=connection)
-# a.pinMode(trigPin, a.OUTPUT)
-# a.pinMode(echoPin, a.INPUT)
+
+tree = LEDBoard(*range(2,28),pwm=True)
+print('what is the tree?')
+#pprint(tree)
+#tree = LEDBoard(2,pwm=True)
+def light_tree():
+  tree.on()
+
+def dim_tree():
+  tree.off()
 
 from nanpy import Ultrasonic
 
 ultrasonic = Ultrasonic(echoPin, trigPin, False, connection=connection)
 
 def test():
-    print(ultrasonic.get_distance())
-    sleep(0.000002)
+    distance = ultrasonic.get_distance()
+    print(distance)
+    if distance < 5:
+        light_tree()
+    else:
+        tree.off()
+    sleep(0.002)
 
 while True:
     test()
